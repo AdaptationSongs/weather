ac0c4d552fe55bc0cb9d809cca3919928c3afa87
Damian Christey <damian.christey@gmail.com>
2018-09-18 12:01:15 -0400

    Use hobo-py library

--- import-hobo-data.py
+++ import-hobo-data.py
@@ -10,7 +10,7 @@ License: GPL
 """
 
 import sys
-import csv
+from hobo import HoboCSVReader
 from time import time
 from datetime import datetime
 from sqlalchemy import Column, Integer, Float, DateTime, Text
@@ -29,7 +29,6 @@ class hobo_data(Base):
     timestamp = Column(DateTime)
     temp = Column(Float)
     rh = Column(Float)
-    dewpt = Column(Float)
 
 # Create the database
 engine = create_engine(sys.argv[1])
@@ -41,40 +40,22 @@ session.configure(bind=engine)
 s = session()
 
 # Read the CSV file
-with open(sys.argv[2]) as file:
-    reader = csv.reader(file)
-    readings = list(reader)
-
-# Get serial number from header row
-first_row = readings[0][0]
-sn_start = first_row.find(':') + 1
-sn_end = first_row.find('-') - 1
-sn = first_row[sn_start:sn_end]
-
-try:
-    t = time()
-    imported = 0
-    existing = 0
-    # Skip first 3 header rows
-    nondata = 3
-    for row in readings[3:]:
-        # Check that this row has numbers in the right places
-        try:
-            timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
-            temp = float(row[1])
-            rh = float(row[2])
-            dewpt = float(row[3])
+with HoboCSVReader(sys.argv[2]) as reader:
+    try:
+        t = time()
+        imported = 0
+        existing = 0
+        for row in reader:
             record = hobo_data(**{
-                'sn' : sn,
-                'timestamp' : timestamp,
-                'temp' : temp,
-                'rh' : rh,
-                'dewpt' : dewpt,
+                'sn' : reader.sn,
+                'timestamp' : row[reader._itimestamp],
+                'temp' : row[reader._itemp],
+                'rh' : row[reader._irh],
             })
             # search for matching records
             match = s.query(hobo_data).filter(
-                hobo_data.sn == sn,
-                hobo_data.timestamp == timestamp
+                hobo_data.sn == record.sn,
+                hobo_data.timestamp == record.timestamp
             )
             if (match.count() > 0):
                  # Skip over existing records
@@ -84,19 +65,14 @@ try:
                 s.add(record)
                 imported += 1
 
-        # Skip over non-numeric data
-        except ValueError:
-            nondata += 1
+        s.commit() # Attempt to commit all the records
 
-    s.commit() # Attempt to commit all the records
+    except Exception as e:
+        print('Something bad happened: {0}'.format(e))
+        s.rollback() # Rollback the changes on error
 
-except Exception as e:
-    print('Something bad happened: {0}'.format(e))
-    s.rollback() # Rollback the changes on error
-
-finally:
-    s.close() # Close the connection
-    print('Imported rows: {0}'.format(imported))
-    print('Nondata rows skipped: {0}'.format(nondata))
-    print('Existing records skipped: {0}'.format(existing))
-    print('Time elapsed: {0} seconds'.format(time() - t))
+    finally:
+        s.close() # Close the connection
+        print('Imported rows: {0}'.format(imported))
+        print('Existing records skipped: {0}'.format(existing))
+        print('Time elapsed: {0} seconds'.format(time() - t))
